1e62f3339d8d50d908fa8ee0bce06cc7fdc23567
Frida Marie Viset <frida.viset@gmail.com>
2022-02-25 10:09:00 +0100

    Add files via upload

---
+++ main.m
@@ -0,0 +1,11 @@
+%Copyright (C) 2022 by Frida Viset
+
+run('tools\load_tools.m');
+run('ModelShipExperiments\main.m');
+run('ModelShipExperiments\VaryOdometryNoise.m');
+run('ModelShipExperiments\RepeatedExperiments.m');
+run('EKFSLAMOpenShoe\main.m');
+run('Simulations\main.m');
+
+
+
