b1f32bc4440f0eca94f2f862fd9e3f0acdcbd8e5
Frida Marie Viset <frida.viset@gmail.com>
2022-02-28 09:47:08 +0100

    Update Simple_Dead_Reckoning.m
    bugfix

--- tools/InertialNavigation/Simple_Dead_Reckoning.m
+++ tools/InertialNavigation/Simple_Dead_Reckoning.m
@@ -1,7 +1,7 @@
 function [p_DR,q_DR]=Simple_Dead_Reckoning(delta_p,delta_q,p_0,q_0)
 %Copyright (C) 2022 by Frida Viset
 
-=size(delta_p,2);
+N=size(delta_p,2);
 p_DR=zeros(3,N);
 q_DR=zeros(4,N);
 p_DR(:,1)=p_0;
