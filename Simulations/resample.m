0c9ccf1377f72502c2bc39f786fc2084ea0371b9
Frida Marie Viset <frida.viset@gmail.com>
2022-04-07 11:55:21 +0200

    Delete Simulations directory

--- Simulations/resample.m
+++
@@ -1,8 +0,0 @@
-function [new_q, new_p]=resample(old_q,old_p,weights)
-%Copyright (C) 2022 by Frida Viset
-
-M=length(weights);
-indices=randsample(M,M,'true',weights);
-new_q=old_q(:,:,indices);
-new_p=old_p(:,:,indices);
-end
